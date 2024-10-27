bba467da2659be4afb4dbc9e68cb913546120894
Ez-114 <morganezz226@gmail.com>
2024-10-27 13:18:48 +0300

    [add] task 1

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
-function getResponseFromaAPI() {
-  return new Promise((resolve, reject) => {
-    const success = true;
-
-    if (success) {
-      resolve("Success!");
-    } else {
-      reject(new Error("API call failed"));
-    }
-  });
+export default function getResponseFromaAPI() {
+  return new Promise(() => {});
 }
