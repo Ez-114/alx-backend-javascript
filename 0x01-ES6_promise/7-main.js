7f80556e1225fcbdb73e86833d425410e7e8283d
Ez-114 <morganezz226@gmail.com>
2024-10-28 23:49:48 +0300

    [mod] changed load_balancer method

--- 0x01-ES6_promise/7-main.js
+++ 0x01-ES6_promise/7-main.js
@@ -2,6 +2,9 @@ import loadBalancer from "./7-load_balancer";
 
 const ukSuccess = 'Downloading from UK is faster';
 const frSuccess = 'Downloading from FR is faster';
+const chinaSuccess = 'Downloading from China is faster';
+const USASuccess = 'Downloading from USA is faster';
+
 
 const promiseUK = new Promise(function (resolve, reject) {
   setTimeout(resolve, 100, ukSuccess);
@@ -15,9 +18,18 @@ const promiseFR = new Promise(function (resolve, reject) {
   setTimeout(resolve, 200, frSuccess);
 });
 
+const promiseChina = new Promise(function(resolve, reject) {
+  setTimeout(resolve, 100, chinaSuccess);
+});
+
+const promiseUSA = new Promise(function(resolve, reject) {
+  setTimeout(resolve, 300, USASuccess);
+});
+
 const test = async () => {
   console.log(await loadBalancer(promiseUK, promiseFR));
   console.log(await loadBalancer(promiseUKSlow, promiseFR));
+  console.log(await loadBalancer(promiseChina, promiseUSA));
 }
 
 test();
