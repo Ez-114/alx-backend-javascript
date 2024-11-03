65627837177e3021f629ef4603c023eebedd9521
Ez-114 <morganezz226@gmail.com>
2024-11-03 17:28:11 +0200

    [mod] task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,6 @@
 export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+
   return Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
