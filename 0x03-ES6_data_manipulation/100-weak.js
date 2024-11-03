51bba7bda7878cde56e4e45503a73cec7ba44ab9
Ez-114 <morganezz226@gmail.com>
2024-11-03 18:10:41 +0200

    [mod] added the number of endpoint requests first then checked for load

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,9 +4,8 @@ export function queryAPI(endpoint) {
     weakMap.set(endpoint, 0);
   }
 
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
-
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 }
