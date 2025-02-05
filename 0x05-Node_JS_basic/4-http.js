92fcf08b8a79b3d356fe12fa983af20c5c4ecd12
Ez-114 <morganezz226@gmail.com>
2025-02-05 18:28:10 +0200

    changed the file content :(

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,29 +1,20 @@
-const http = require('node:http');
+const http = require('http');
 
-// Define the host and port
-const HOST = 'localhost';
 const PORT = 1245;
-
-// Create a server instance
+const HOST = 'localhost';
 const app = http.createServer();
 
-// Listen to any request
-app.on('request', (request, response) => {
-  // Log the requested url
-  process.stdout.write(`New Request: ${request.url}\n`);
-
+app.on('request', (_, res) => {
   const responseText = 'Hello Holberton School!';
 
-  // Set response headers and status code
-  response.setHeader('Content-Type', 'text/plain');
-  response.setHeader('Content-Length', responseText.length);
-  response.statusCode = 200;
-  response.write(responseText);
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
-// Start listening
 app.listen(PORT, HOST, () => {
-  process.stdout.write(`Server started -> http://${HOST}:${PORT}/\n`);
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
 module.exports = app;
